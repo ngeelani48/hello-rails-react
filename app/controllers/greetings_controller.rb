ab622633c037ec43f6e468d7a6a497d620e927cb
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-07-11 09:45:54 +0300

    Setup controller files and installed dependies

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,2 +1,6 @@
 class GreetingsController < ApplicationController
+  def random_greeting
+    @random_greeting = Message.order('RANDOM()').first
+    render json: { greeting: @random_greeting }
+  end
 end
